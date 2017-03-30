3fe14862edc85b0753315f146deb3ae59dcd5554
Lee Dale <lee.jdale@gmail.com>
2017-03-30 16:56:06 +0100

    Testing CI4

--- src/models/metoffice.ts
+++ src/models/metoffice.ts
@@ -203,8 +203,8 @@ export class WeatherText {
     }
 
     public static GetFlyingTextFromForecast(forecast: Forecast): string {
-        let yesResponseText: string = "Testing 3! Yes you can fly today.";
-        let noResponseText: string = "Testing 3! You should not fly today.";
+        let yesResponseText: string = "Testing 4! Yes you can fly today.";
+        let noResponseText: string = "Testing 4! You should not fly today.";
         let responseText: string;
 
         switch (forecast.weatherType) {
